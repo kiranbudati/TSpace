f971b761b9a2562ead6cf916fb4d2effc580eecd
kiran <kiranreddybudati503@gmail.com>
2018-08-17 08:14:43 -0700

    windows

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
 from bs4 import BeautifulSoup
 import time
 from selenium import webdriver
+# import pymongo
+# from pymongo import MongoClient
+
+# client = MongoClient('ec2-18-188-183-169.us-east-2.compute.amazonaws.com', 27017)
+
+# db = client.tspace
 
 url = "http://www.isstracker.com"
 browser = webdriver.Chrome("D:\Goals\Projects\ISS\chromedriver.exe")
 browser.get(url)
 while True:
     time.sleep(1.5)
+    # ts = time.time()
     html = browser.page_source
     soup = BeautifulSoup(html, "lxml")
     longitude_field = soup.find('p',attrs={'id':'longitudeValue'})
@@ -17,6 +24,7 @@ while True:
     longitude = longitude_field.text.strip()
     khm = khm_field.text.strip()
 
-    print("latitude : "+ latitude+" longitude: "+longitude+" khm: "+khm)
+    # db.iss.insert_one({"latitude":latitude,"longitude":longitude,"khm":khm})
+    print("latitude : "+ latitude+" longitude: "+longitude+" khm: "+khm+" time: ",time.time())
 # browser.close()
 # browser.quit()
